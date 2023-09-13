5b9ac19cda2b882f5f66d29953251cb4f2cdf881
Promise12345678 <promisesmutenhaunga@gmail.com>
2023-09-12 17:12:57 -0700

    Attack

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
-});
 
+const newList = list.map((el, idx) => el * idx);
 console.log(list);
 console.log(newList);
